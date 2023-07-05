af5138b6ef56c1db5344a532567cffff0d47dcad
aurelo <zlatko.gudasic@gmail.com>
2023-07-05 13:40:43 +0200

    first commit

---
+++ src/main/java/UuidTimestampGenerator.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class UuidTimestampGenerator {
+    public static String generate() {
+        return String.format("%s => %s", LocalDate.now(), UUID.randomUUID().toString());
+    }
+}
